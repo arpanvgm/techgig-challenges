df9d81873e9f4a3758563a1e67d1dff637dab091
Arpan Shah <arpankumarshah@gmail.com>
2023-02-23 22:44:03 +0530

    Prime Code  Champ Solved

---
+++ Prime-Code-Champ-Techgig/PickLuckyBox/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickLuckyBox
+{
+    internal class Program
+    {
+		static void Main(string[] args)
+		{
+			//Write code here
+			var size = short.Parse(Console.ReadLine());
+			var table = new int[size, size];
+			for (short i = 0; i < size; i++)
+			{
+				var inputs = Console.ReadLine().Split(new char[] { '#' }); // To capture money value of blocks.
+				for (short j = 0; j < size; j++)
+				{
+					table[i, j] = int.Parse(inputs[j]); // assigning money value to respetive blocks.
+				}
+			}
+			Func<short, short, int> findMinimumPosibleMoneyAmount = (ro, co) => {
+				int minValue = int.MaxValue;
+				for (var i = ro - 1; i < ro + 2; i++)
+				{
+					if (i < 0 || i >= size)
+						continue;
+					for (var j = co - 1; j < co + 2; j++)
+					{
+						if (j < 0 || j >= size)
+							continue;
+						//Console.Write("({0})",table[i,j]);
+						if (minValue > table[i, j])
+						{
+							minValue = table[i, j];
+						}
+					}
+				}
+				//Console.Write(minValue);
+				return minValue;
+			}; // Func expression closed 
+
+			var blocks = new Block[size * size];
+			short count = 0;
+			for (short r = 0; r < size; r++)
+			{
+				for (short c = 0; c < size; c++)
+				{
+					blocks[count] = new Block();
+					//Console.Write("{0}#{1} : ",r,c);
+					blocks[count].Value = findMinimumPosibleMoneyAmount(r, c);
+					blocks[count].rowNo = r;
+					blocks[count].columnNo = c;
+					//Console.WriteLine();
+					count++;
+				}
+			}
+			var filtered = blocks.Where(c => c.Value == blocks.Max(b => b.Value));
+			foreach (var b in filtered)
+			{
+				Console.WriteLine("{0}#{1}", b.rowNo + 1, b.columnNo + 1);
+			}
+
+
+		} // close main
+		class Block // private class 
+		{
+			public short rowNo { get; set; }
+			public short columnNo { get; set; }
+			public int Value { get; set; }
+		}
+	}
+}
---
+++ Prime-Code-Champ-Techgig/PickLuckyBox/Readme.txt
@@ -0,0 +1,16 @@
+﻿Game Center (100 Marks)
+You and your friend go to a game arcade where you choose to play the game Lucky Pick. In the game, there is a square grid and on each block, some money is placed on it. When a player chooses a block, the machine randomly chooses a block from the neighboring ones and the chosen block (consider 8 neighborhood). The player is awarded the money that is placed on the block that the machine selects. Your friend needs help choosing the block.
+
+Your job is to return the block position(s) that will maximize the minimum amount your friend will win for sure. If there are more than one such block positions then the output must return for all these positions.
+
+Input Format
+You will be given the Grid Description as -
+The first line consists of the size of the square grid (N)
+The next N lines each containing N numbers separated by '#', each number representing the amount of money put on that block
+
+
+Constraints
+1 < N < 500
+
+Output Format
+You need to print the array of string containing the position(s) of a block choosing which will give the maximum amount of money which your friend will definitely win.
